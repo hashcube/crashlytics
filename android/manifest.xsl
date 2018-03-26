01383d299a896a2987b1eda32eb83adc8ed35c1d
Syed zainul abedin <szsimplesyed93@gmail.com>
2018-03-26 15:42:11 +0530

    Copying shell script to xcode project for adding in build phases

--- android/manifest.xsl
+++ android/manifest.xsl
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:android="http://schemas.android.com/apk/res/android">
 
-  <xsl:param name="crashlyticsAppKey" />
+  <xsl:param name="crashlytics_key" />
 
   <xsl:output indent="yes" />
   <xsl:template match="comment()" />
 
   <xsl:template match="meta-data[@android:name='io.fabric.ApiKey']">
-    <meta-data android:name="io.fabric.ApiKey" android:value="{$crashlyticsAppKey}" />
+    <meta-data android:name="io.fabric.ApiKey" android:value="{$crashlytics_key}" />
   </xsl:template>
 
   <xsl:output indent="yes" />
--- build/index.js
+++ build/index.js
@@ -5,7 +5,8 @@ var xmldoc = require('xmldoc');
 
 exports.onCreateProject = function (api, app, config, cb) {
 
-  var copyCustomRules = function (outpath) {
+  var outputPath = config.outputPath,
+  copyCustomRules = function (outpath) {
     var file = 'custom_rules.xml',
       srcPath = path.join(__dirname, '../android', file),
       buildXml = fs.readFileSync(srcPath, "utf-8"),
@@ -18,6 +19,16 @@ exports.onCreateProject = function (api, app, config, cb) {
     return fs.outputFileAsync(path.join(outpath, file), xmlDoc.toString(), 'utf-8');
   };
 
-  return copyCustomRules(config.outputPath)
-    .then(cb);
-}; 
+  if (config.target === 'native-android') {
+    return copyCustomRules(outputPath)
+      .then(cb);
+  } else if (config.target === 'native-ios') {
+    var buildScipt = path.join(__dirname, '../ios', "buildScript"),
+      manifest = app.manifest.ios,
+      scriptContent = fs.readFileSync(buildScipt, "utf-8");
+
+    return fs.outputFileAsync(path.join(outputPath, "resources/extra"),
+      scriptContent.toString() + manifest.crashlytics_key + " " + manifest.crashlytics_extra_key,'utf-8')
+      .then(cb);
+  }
+};
